cc48b4d293a4ec0ef76dd682873d8956fb2c3253
ivanpriyambudi <gendoh75@gmail.com>
2019-11-29 17:39:17 +0700

    delete file gak penting

--- resources/views/landing/app/app.blade.php
+++ resources/views/landing/app/app.blade.php
@@ -4,13 +4,6 @@
 
   @yield('title')
 
-  <!--  favicon -->
-  <link rel="shortcut icon" href="{{asset('assets/img/ico/favicon.png')}}">
-  <!--  apple-touch-icon -->
-  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{asset('assets/img/ico/apple-touch-icon-144-precomposed.png')}}">
-  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{asset('assets/img/ico/apple-touch-icon-114-precomposed.png')}}">
-  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{asset('assets/img/ico/apple-touch-icon-72-precomposed.png')}}">
-  <link rel="apple-touch-icon-precomposed" href="{{asset('assets/img/ico/apple-touch-icon-57-precomposed.png')}}">
 
   <link href='https://fonts.googleapis.com/css?family=Raleway:400,300,500,700,900' rel='stylesheet' type='text/css'>
   <!-- Material Icons CSS -->
